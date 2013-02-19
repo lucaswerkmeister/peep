535b70c5795e6d2fdbb5d545136525644a3b4260
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-02-19 10:47:16 +0100

    Initial commit of the PEEP Git plugin.

---
+++ Project Euler Eclipse Plugin – Git Connector/src/de/lucaswerkmeister/peep/git/CreateBranch.java
@@ -0,0 +1,11 @@
+package de.lucaswerkmeister.peep.git;
+
+import de.lucaswerkmeister.peep.extensionPoints.PeepTask;
+
+public class CreateBranch implements PeepTask {
+
+	@Override
+	public void execute(int problemNumber) {
+		// TODO Auto-generated method stub
+	}
+}
