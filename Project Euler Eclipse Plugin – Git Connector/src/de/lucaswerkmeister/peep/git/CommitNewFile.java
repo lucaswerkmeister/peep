16e89758436044b9cbe5f8d550c4e732fa9e66ec
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-02-19 15:01:01 +0100

    Fixed the CommitNewFile connector.

--- Project Euler Eclipse Plugin – Git Connector/src/de/lucaswerkmeister/peep/git/CommitNewFile.java
+++ Project Euler Eclipse Plugin – Git Connector/src/de/lucaswerkmeister/peep/git/CommitNewFile.java
@@ -2,8 +2,6 @@ package de.lucaswerkmeister.peep.git;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.egit.core.project.RepositoryFinder;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -16,14 +14,13 @@ public class CommitNewFile implements CreateTask {
 	@Override
 	public void execute(IProject project, IFile file, int problemNumber) {
 		try {
-			RepositoryMapping m = new RepositoryFinder(project).find(null).iterator().next();
-			Repository r = m.getRepository();
-			Git git = new Git(r);
+			Repository repo = RepositoryMapping.getMapping(project).getRepository();
+			Git git = new Git(repo);
 			String problemString = "Problem" + String.format("%03d", problemNumber);
 			git.add().addFilepattern(file.getProjectRelativePath().toString()).call();
-			git.commit().setMessage("Initial commit for " + problemString).call();
+			git.commit().setMessage("Initial commit for " + problemString + ".").call();
 		}
-		catch (CoreException | GitAPIException e) {
+		catch (GitAPIException e) {
 			e.printStackTrace();
 		}
 	}
