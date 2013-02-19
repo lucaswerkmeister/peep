5336a534fbb026cff72751f5530886dd904d1c34
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-02-19 11:37:09 +0100

    Added a new extension in the Git plugin.

---
+++ Project Euler Eclipse Plugin – Git Connector/src/de/lucaswerkmeister/peep/git/CommitNewFile.java
@@ -0,0 +1,29 @@
+package de.lucaswerkmeister.peep.git;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.egit.core.project.RepositoryFinder;
+import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+
+import de.lucaswerkmeister.peep.core.extensionPoints.PeepTask;
+
+public class CommitNewFile implements PeepTask {
+
+	@Override
+	public void execute(IProject project, int problemNumber) {
+		try {
+			RepositoryMapping m = new RepositoryFinder(project).find(null).iterator().next();
+			Repository r = m.getRepository();
+			Git git = new Git(r);
+			String problemString = "Problem" + String.format("%03d", problemNumber);
+			git.add().addFilepattern(problemString).call();
+			git.commit().setMessage("Initial commit for " + problemString).call();
+		}
+		catch (CoreException | GitAPIException e) {
+			e.printStackTrace();
+		}
+	}
+}
