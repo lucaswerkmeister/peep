46055ead67bfe00741ebb0ec005746d6922f6139
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-02-19 10:30:36 +0100

    Added a second extension point and running them both.

--- Project Euler Eclipse Plugin/src/de/lucaswerkmeister/peep/wizards/NewProblemWizard.java
+++ Project Euler Eclipse Plugin/src/de/lucaswerkmeister/peep/wizards/NewProblemWizard.java
@@ -16,9 +16,13 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.ISafeRunnable;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.SafeRunner;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
@@ -33,25 +37,24 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 
+import de.lucaswerkmeister.peep.extensionPoints.PeepTask;
 import de.lucaswerkmeister.peep.pages.ProblemNumberPage;
 
 /**
- * This wizard creates a new class from a user-defined template in the same
- * directory by replacing certain tags of said template with actual content. The
- * currently supported tags are:
+ * This wizard creates a new class from a user-defined template in the same directory by replacing certain tags of said
+ * template with actual content. The currently supported tags are:
  * <ul>
- * <li><code>&PROBLEMNUMBER;</code> the problem number, three decimal digits
- * (015).</li>
- * <li><code>&USERNAME;</code> the user name, just as in eclipse's variable
- * ${USER}.</li>
- * <li><code>&PROBLEMTEXT_HTML;</code> the problem's text, fetched from the
- * projecteuler.net page.</li>
+ * <li><code>&PROBLEMNUMBER;</code> the problem number, three decimal digits (015).</li>
+ * <li><code>&USERNAME;</code> the user name, just as in eclipse's variable ${USER}.</li>
+ * <li><code>&PROBLEMTEXT_HTML;</code> the problem's text, fetched from the projecteuler.net page.</li>
  * </ul>
  */
 
 public class NewProblemWizard extends Wizard implements INewWizard {
-	private ProblemNumberPage page;
-	private ISelection selection;
+	private ProblemNumberPage	page;
+	private ISelection			selection;
+	private static final String	BEFORE_CREATE_ID	= "de.lucaswerkmeister.peep.core.beforeCreate";
+	private static final String	AFTER_CREATE_ID		= "de.lucaswerkmeister.peep.core.afterCreate";
 
 	/**
 	 * Constructor for NewProblemWizard.
@@ -70,8 +73,8 @@ public class NewProblemWizard extends Wizard implements INewWizard {
 	}
 
 	/**
-	 * This method is called when the 'Finish' button is pressed in the wizard.
-	 * We will create an operation and run it using wizard as execution context.
+	 * This method is called when the 'Finish' button is pressed in the wizard. We will create an operation and run it
+	 * using wizard as execution context.
 	 */
 	public boolean performFinish() {
 		final String containerName = page.getContainerName();
@@ -82,18 +85,22 @@ public class NewProblemWizard extends Wizard implements INewWizard {
 					throws InvocationTargetException {
 				try {
 					doFinish(containerName, fileName, problemNumber, monitor);
-				} catch (CoreException e) {
+				}
+				catch (CoreException e) {
 					throw new InvocationTargetException(e);
-				} finally {
+				}
+				finally {
 					monitor.done();
 				}
 			}
 		};
 		try {
 			getContainer().run(true, false, op);
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e) {
 			return false;
-		} catch (InvocationTargetException e) {
+		}
+		catch (InvocationTargetException e) {
 			Throwable realException = e.getTargetException();
 			MessageDialog.openError(getShell(), "Error",
 					realException.getMessage());
@@ -102,15 +109,41 @@ public class NewProblemWizard extends Wizard implements INewWizard {
 		return true;
 	}
 
+	private void runTasks(final String extensionPointId, final int problemNumber) {
+		for (IConfigurationElement e : Platform.getExtensionRegistry().getConfigurationElementsFor(extensionPointId)) {
+			final Object extension;
+			try {
+				extension = e.createExecutableExtension("class");
+			}
+			catch (CoreException e1) {
+				continue;
+			}
+			if (extension instanceof PeepTask)
+				SafeRunner.run(new ISafeRunnable() {
+					@Override
+					public void run() throws Exception {
+						((PeepTask) extension).execute(problemNumber);
+					}
+
+					@Override
+					public void handleException(Throwable exception) {
+						// Do nothing
+					}
+				});
+		}
+	}
+
 	/**
-	 * The worker method. It will find the container, create the file if missing
-	 * or just replace its contents, and open the editor on the newly created
-	 * file.
+	 * The worker method. It will find the container, create the file if missing or just replace its contents, and open
+	 * the editor on the newly created file.
 	 */
 	private void doFinish(String containerName, String fileName,
 			int problemNumber, IProgressMonitor monitor) throws CoreException {
 		// create a sample file
-		monitor.beginTask("Creating " + fileName, 2);
+		monitor.beginTask("Creating " + fileName, 4);
+		monitor.setTaskName("Running extensions");
+		runTasks(BEFORE_CREATE_ID, problemNumber);
+		monitor.worked(1);
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		IResource resource = root.findMember(new Path(containerName));
 		if (!resource.exists() || !(resource instanceof IContainer)) {
@@ -119,42 +152,44 @@ public class NewProblemWizard extends Wizard implements INewWizard {
 		}
 		IContainer container = (IContainer) resource;
 		final IFile file = container.getFile(new Path(fileName));
-		try (InputStream stream = openContentStream(problemNumber, container)) {
+		try (InputStream stream = openContentStream(problemNumber, container, monitor)) {
 			if (file.exists()) {
 				file.setContents(stream, true, true, monitor);
-			} else {
+			}
+			else {
 				file.create(stream, true, monitor);
 			}
 			stream.close();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 		}
 		monitor.worked(1);
-		monitor.setTaskName("Opening file for editing...");
+		monitor.setTaskName("Running extensions");
+		runTasks(AFTER_CREATE_ID, problemNumber);
 		getShell().getDisplay().asyncExec(new Runnable() {
 			public void run() {
 				IWorkbenchPage page = PlatformUI.getWorkbench()
 						.getActiveWorkbenchWindow().getActivePage();
 				try {
 					IDE.openEditor(page, file, true);
-				} catch (PartInitException e) {
+				}
+				catch (PartInitException e) {
 				}
 			}
 		});
-		monitor.worked(1);
 	}
 
 	/**
-	 * We initialize file contents with the template contents and then replace
-	 * the variables.
+	 * We initialize file contents with the template contents and then replace the variables.
 	 * 
 	 * @throws FileNotFoundException
 	 *             If the template file doesn't exist.
 	 */
 	private InputStream openContentStream(int problemNumber,
-			IContainer container) throws FileNotFoundException {
+			IContainer container, IProgressMonitor monitor) throws FileNotFoundException {
 		String contents = null;
 		byte readTimeoutCounter = 0;
-
+		monitor.setTaskName("Creating file");
 		do {
 			IResource templateFile = container.findMember("/Problem.template");
 			if (templateFile == null)
@@ -166,9 +201,11 @@ public class NewProblemWizard extends Wizard implements INewWizard {
 			}
 			readTimeoutCounter++;
 		} while (contents == null && readTimeoutCounter < 10);
+		monitor.worked(1);
 		if (contents == null)
 			return new ByteArrayInputStream(
 					"Unable to find template file, sorry.".getBytes());
+		monitor.setTaskName("Decorating file");
 		if (contents.contains("&PROBLEMTEXT_HTML;")) {
 			String problemText_html = null;
 			readTimeoutCounter = 0;
@@ -199,9 +236,11 @@ public class NewProblemWizard extends Wizard implements INewWizard {
 					problemText_html = problemText_html.trim();
 
 					readTimeoutCounter++;
-				} catch (MalformedURLException e) {
+				}
+				catch (MalformedURLException e) {
 					e.printStackTrace();
-				} catch (IOException e) {
+				}
+				catch (IOException e) {
 					e.printStackTrace();
 				}
 			} while (problemText_html == null && readTimeoutCounter < 10);
@@ -234,6 +273,8 @@ public class NewProblemWizard extends Wizard implements INewWizard {
 		contents = contents.replaceAll("&PROBLEMLINK;",
 				"http://www.projecteuler.net/problem=" + problemNumber);
 
+		monitor.worked(1);
+
 		return new ByteArrayInputStream(contents.getBytes());
 	}
 
@@ -244,8 +285,7 @@ public class NewProblemWizard extends Wizard implements INewWizard {
 	}
 
 	/**
-	 * We will accept the selection in the workbench to see if we can initialize
-	 * from it.
+	 * We will accept the selection in the workbench to see if we can initialize from it.
 	 * 
 	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
 	 */
