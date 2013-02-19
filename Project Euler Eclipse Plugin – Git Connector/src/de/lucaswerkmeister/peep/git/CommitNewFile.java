9ae7d8dde8b8e74d619fb5e9c3baee127a8edcb7
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-02-19 18:25:43 +0100

    Fixed a bug in the Git connector.
    The relative path of the new file was wrong, so that the wrong path was
    added and the new file not committed.

--- Project Euler Eclipse Plugin – Git Connector/src/de/lucaswerkmeister/peep/git/CommitNewFile.java
+++ Project Euler Eclipse Plugin – Git Connector/src/de/lucaswerkmeister/peep/git/CommitNewFile.java
@@ -2,6 +2,8 @@ package de.lucaswerkmeister.peep.git;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -17,7 +19,8 @@ public class CommitNewFile implements CreateTask {
 			Repository repo = RepositoryMapping.getMapping(project).getRepository();
 			Git git = new Git(repo);
 			String problemString = "Problem" + String.format("%03d", problemNumber);
-			git.add().addFilepattern(file.getProjectRelativePath().toString()).call();
+			IPath path = file.getFullPath().makeRelativeTo(new Path(repo.getDirectory().getPath()));
+			git.add().addFilepattern(path.toString()).call();
 			git.commit().setMessage("Initial commit for " + problemString + ".").call();
 		}
 		catch (GitAPIException e) {
