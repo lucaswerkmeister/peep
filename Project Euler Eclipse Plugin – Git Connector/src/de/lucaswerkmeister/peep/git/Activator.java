c563bad029f1869f1b4135884cdc1304036a7c14
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-02-19 11:37:18 +0100

    Reformatted the activators.

--- Project Euler Eclipse Plugin – Git Connector/src/de/lucaswerkmeister/peep/git/Activator.java
+++ Project Euler Eclipse Plugin – Git Connector/src/de/lucaswerkmeister/peep/git/Activator.java
@@ -5,7 +5,7 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+	private static BundleContext	context;
 
 	static BundleContext getContext() {
 		return context;
@@ -13,6 +13,7 @@ public class Activator implements BundleActivator {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
@@ -21,6 +22,7 @@ public class Activator implements BundleActivator {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
